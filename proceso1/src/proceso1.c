56cd74621566e699c446493a29129a37087ba999
nicolas <nfernndez@frba.utn.edu.ar>
2023-08-21 04:18:12 +0000

    First

---
+++ proceso1/src/proceso1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : proceso1.c
+ Author      : nicofernandez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!HolaMundo!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
